dceabfcbf7461367cbb12b3ed259f29687b50dac
CeliaDawn <mynameisaverb1@gmail.com>
2024-04-06 16:46:41 -0400

    Map editing r1

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1362,3 +1362,6 @@ Text_TestMsg::
 	.include "data/text/save.inc"
 	.include "data/text/new_game_intro.inc"
 	.include "data/text/pokedude.inc"
+
+	.include "data/maps/NewMap1/scripts.inc"
+	.include "data/maps/NewMap1/text.inc"
