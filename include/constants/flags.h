d345b6547f9975b0c8f85b1302e4bda82b989e98
CeliaDawn <mynameisaverb1@gmail.com>
2024-08-23 11:00:00 -0400

    fixed your flags

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1257,8 +1257,8 @@
 #define FLAG_HIDDEN_ITEM_SSANNE_EXTERIOR_LAVA_COOKIE                       (FLAG_HIDDEN_ITEMS_START + 190)
 
 // Unused hidden item flags
-#define FLAG_UNUSED_0x4A7               0x4A7 // Unused Flag
-#define FLAG_UNUSED_0x4A8               0x4A8 // Unused Flag
+#define FLAG_CSR_ROUTE_4_SIRTUS               0x4A7 // Unused Flag
+#define FLAG_CSR_VIRIDIAN_HIDDEN_ETHER               0x4A8 // Unused Flag
 #define FLAG_UNUSED_0x4A9               0x4A9 // Unused Flag
 #define FLAG_UNUSED_0x4AA               0x4AA // Unused Flag
 #define FLAG_UNUSED_0x4AB               0x4AB // Unused Flag
