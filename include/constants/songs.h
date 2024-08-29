78b7a2c5c27aca677b483526b976d623ce555f4f
CeliaDawn <mynameisaverb1@gmail.com>
2024-08-29 00:51:15 -0400

    Loss Woods

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -362,10 +362,11 @@
 #define MUS_CSR_STAIRS            353
 #define MUS_CSR_CASTLE            354
 #define MUS_CSR_DRIFTVEIL            355
+#define MUS_CSR_LOSS_WOODS            356
 
 #define MUS_NONE                    0xFFFF
 
 #define START_MUS                   MUS_HEAL
-#define END_MUS                     MUS_CSR_DRIFTVEIL
+#define END_MUS                     MUS_CSR_LOSS_WOODS
 
 #endif  // GUARD_CONSTANTS_SONGS_H
