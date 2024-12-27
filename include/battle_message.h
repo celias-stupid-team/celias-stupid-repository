087a3364d7a32de6d5b869468b2333828edc7ad0
RavePossum <ravepossum@proton.me>
2024-12-27 09:02:44 -0700

    Avoid another long move name buffer overflow, unify long name constant usage

--- include/battle_message.h
+++ include/battle_message.h
@@ -193,7 +193,7 @@
     textVar[4] = B_BUFF_EOS;                                    \
 }
 
-#define TEXT_BUFF_ARRAY_COUNT   20 // for long move names (18) + potential "!" + EOS
+#define TEXT_BUFF_ARRAY_COUNT (LONG_MOVE_NAME_LENGTH + 2) // for long move names (17) + potential "!" + EOS
 
 struct BattleMsgData
 {
