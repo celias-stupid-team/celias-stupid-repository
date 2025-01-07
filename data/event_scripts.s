0513bf5052f3588c147065e88f66c9adf5ea99ab
CeliaDawn <mynameisaverb1@gmail.com>
2025-01-06 23:38:19 -0500

    infinite staircase

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1398,3 +1398,6 @@ Text_TestMsg::
 
 	.include "data/maps/LavenderTown_FlowerShop/scripts.inc"
 	.include "data/maps/LavenderTown_FlowerShop/text.inc"
+
+	.include "data/maps/CeladonCity_InfiniteStaircase/scripts.inc"
+	.include "data/maps/CeladonCity_InfiniteStaircase/text.inc"
