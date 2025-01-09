5099023c42eb2261b922e15c4f8bfdbc1c67bd85
CeliaDawn <mynameisaverb1@gmail.com>
2025-01-09 18:01:19 -0500

    Types re-reorganized

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -99,24 +99,24 @@
 #define TYPE_FIGHTING       1
 #define TYPE_FLYING         2
 #define TYPE_POISON         3
-#define TYPE_GROUND         4
+#define TYPE_STEEL          4
 #define TYPE_ROCK           5
 #define TYPE_BUG            6
 #define TYPE_GHOST          7
-#define TYPE_STEEL          8
-#define TYPE_MYSTERY        9
-#define TYPE_FIRE           10
-#define TYPE_WATER          11
-#define TYPE_GRASS          12
-#define TYPE_ELECTRIC       13
-#define TYPE_PSYCHIC        14
-#define TYPE_ICE            15
-#define TYPE_DRAGON         16
-#define TYPE_DARK           17
-#define TYPE_FAIRY          18
-#define TYPE_WATER_PHYSICAL 19
-#define TYPE_ELECTRIC_PHYSICAL 20
-#define TYPE_PSYCHIC_PHYSICAL 21
+#define TYPE_WATER_PHYSICAL  8
+#define TYPE_ELECTRIC_PHYSICAL 9
+#define TYPE_PSYCHIC_PHYSICAL 10
+#define TYPE_MYSTERY        11 //Special types below!
+#define TYPE_GROUND         12 //Yes Ground is special in this game :)
+#define TYPE_FIRE           13
+#define TYPE_WATER          14
+#define TYPE_GRASS          15
+#define TYPE_ELECTRIC       16
+#define TYPE_PSYCHIC        17
+#define TYPE_ICE            18
+#define TYPE_DRAGON         19
+#define TYPE_DARK           20
+#define TYPE_FAIRY          21
 #define TYPE_BROCK          22
 #define TYPE_WEIRD          23
 #define TYPE_DAD            24
@@ -130,7 +130,6 @@
 #define TYPE_GRASS_TCG          32
 
 #define NUMBER_OF_MON_TYPES 33
-
 // Pokemon egg groups
 #define EGG_GROUP_NONE         0
 #define EGG_GROUP_MONSTER      1
