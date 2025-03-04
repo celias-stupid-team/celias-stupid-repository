1c54fc057af8aa508aaf488eeef10911c04c184a
CeliaDawn <mynameisaverb1@gmail.com>
2025-03-03 21:39:16 -0500

    haircut dialogue

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -320,8 +320,8 @@
 #define FLAG_CSR_ROUTE12_BROTHER_IS_CHASING               0x112
 #define FLAG_CSR_SOUL_DEW_FOUND               0x113
 #define FLAG_CSR_CAUGHT_AMPHAROS               0x114
-#define FLAG_0x115               0x115
-#define FLAG_0x116               0x116
+#define FLAG_CUT_AMPHAROS_HAIR               0x115
+#define FLAG_CUT_TANGELA_HAIR               0x116
 #define FLAG_0x117               0x117
 #define FLAG_0x118               0x118
 #define FLAG_0x119               0x119
