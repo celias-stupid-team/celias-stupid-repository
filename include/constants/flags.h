7b3e55b1bb92ba46e5ccf81dfa2864ec2e9eb6b1
CeliaDawn <mynameisaverb1@gmail.com>
2025-03-11 22:37:41 -0400

    pissballs

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -324,7 +324,7 @@
 #define FLAG_CUT_TANGELA_HAIR               0x116
 #define FLAG_NO_TM_CASE               0x117
 #define FLAG_POKEMONTOWER_POKEBALL               0x118
-#define FLAG_0x119               0x119
+#define FLAG_POKEMONTOWER_BANETTE               0x119
 #define FLAG_0x11A               0x11A
 #define FLAG_0x11B               0x11B
 #define FLAG_0x11C               0x11C
