a31cea63f71fb2e7759a981ff37018bf6f119db6
CeliaDawn <mynameisaverb1@gmail.com>
2025-03-24 18:58:17 -0400

    rest houses

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -333,7 +333,7 @@
 #define FLAG_INSERTED_INSTINCT_SEAL               0x11F
 #define FLAG_GOT_MATH_CLUB               0x120
 #define FLAG_BATTLED_IRON_CLEFTS               0x121
-#define FLAG_0x122               0x122
+#define FLAG_TALKED_TO_FISHING_GURU               0x122
 #define FLAG_0x123               0x123
 #define FLAG_0x124               0x124
 #define FLAG_0x125               0x125
