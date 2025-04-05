cedf38129f0b0e7553b6d63ab59f62536f5a34f3
CeliaDawn <mynameisaverb1@gmail.com>
2025-04-04 21:53:19 -0400

    first playtest cleanup

--- src/data/object_events/object_event_graphics_info.h
+++ src/data/object_events/object_event_graphics_info.h
@@ -4824,7 +4824,7 @@ const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Birdcultist = {
 
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Tentacool = { 
     .tileTag = 0xFFFF,
-    .paletteTag = OBJ_EVENT_PAL_TAG_NPC_PINK,
+    .paletteTag = OBJ_EVENT_PAL_TAG_NPC_BLUE,
     .reflectionPaletteTag = OBJ_EVENT_PAL_TAG_NONE,
     .size = 256,
     .width = 16,
