71c3c4d9446af76f970101872440a2c3b9bcce60
Jonathan Zentgraf <jonathan@zentgraf.dev>
2025-04-06 13:32:48 -0400

    Fix mismatched line endings
    Convert CRLF to LF in files that were previously using LF exclusively.
    Heck, object_event_pic_tables.h somehow had CR with no LF at all...
    This fixes an issue where Git would confuse itself into thinking that
    files had been changed even when this was not the case.

--- src/data/object_events/object_event_pic_tables.h
+++ src/data/object_events/object_event_pic_tables.h
@@ -3065,4 +3065,13 @@ const struct SpriteFrameImage gObjectEventPicTable_Cannon[] = {
 };
 
 const struct SpriteFrameImage gObjectEventPicTable_Spotlight[] = {
-    overworld_frame(gObjectEventPic_Spotlight, 8, 8, 0),    overworld_frame(gObjectEventPic_Spotlight, 8, 8, 1),    overworld_frame(gObjectEventPic_Spotlight, 8, 8, 2),    overworld_frame(gObjectEventPic_Spotlight, 8, 8, 3),    overworld_frame(gObjectEventPic_Spotlight, 8, 8, 4),    overworld_frame(gObjectEventPic_Spotlight, 8, 8, 5),    overworld_frame(gObjectEventPic_Spotlight, 8, 8, 6),    overworld_frame(gObjectEventPic_Spotlight, 8, 8, 7),    overworld_frame(gObjectEventPic_Spotlight, 8, 8, 8),};
+    overworld_frame(gObjectEventPic_Spotlight, 8, 8, 0),
+    overworld_frame(gObjectEventPic_Spotlight, 8, 8, 1),
+    overworld_frame(gObjectEventPic_Spotlight, 8, 8, 2),
+    overworld_frame(gObjectEventPic_Spotlight, 8, 8, 3),
+    overworld_frame(gObjectEventPic_Spotlight, 8, 8, 4),
+    overworld_frame(gObjectEventPic_Spotlight, 8, 8, 5),
+    overworld_frame(gObjectEventPic_Spotlight, 8, 8, 6),
+    overworld_frame(gObjectEventPic_Spotlight, 8, 8, 7),
+    overworld_frame(gObjectEventPic_Spotlight, 8, 8, 8),
+};
