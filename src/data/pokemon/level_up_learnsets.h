fc1d8c8988b77a1992d4687b1f48e1ce6b6ebe8b
CeliaDawn <mynameisaverb1@gmail.com>
2025-06-18 10:33:03 -0400

    bite

--- src/data/pokemon/level_up_learnsets.h
+++ src/data/pokemon/level_up_learnsets.h
@@ -9929,6 +9929,7 @@ static const struct LevelUpMove sTHATSALLLevelUpLearnset[] = {
 static const struct LevelUpMove sKANGASKHANTEACHERLevelUpLearnset[] = {
 	LEVEL_UP_MOVE(1, MOVE_COMET_PUNCH),
 	LEVEL_UP_MOVE(1, MOVE_SUBSTITUTE_2),
+	LEVEL_UP_MOVE(1, MOVE_BITE),
 	LEVEL_UP_END
 };
 
