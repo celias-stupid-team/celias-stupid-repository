5afe318958ae4cf5e6fa438611a86b3ab655e9f9
CeliaDawn <mynameisaverb1@gmail.com>
2025-07-07 17:06:08 -0400

    shorts mail

--- src/script_pokemon_util.c
+++ src/script_pokemon_util.c
@@ -79,6 +79,38 @@ u8 ScriptGiveMon(u16 species, u8 level, u16 item, u32 unused1, u32 unused2, u8 u
     return sentToPc;
 }
 
+
+u8 ScriptGiveShortsEkans(u16 species, u8 level, u16 item, u32 unused1, u32 unused2, u8 unused3)
+{
+    u16 nationalDexNum;
+    int sentToPc;
+    u8 heldItem[2];
+    struct Pokemon *mon = AllocZeroed(sizeof(struct Pokemon));
+
+    CreateMon(mon, species, level, 32, 0, 0, OT_ID_PLAYER_ID, 0);
+    heldItem[0] = item;
+    heldItem[1] = item >> 8;
+    SetMonData(mon, MON_DATA_HELD_ITEM, heldItem);
+    sentToPc = GiveMonToPlayer(mon);
+    nationalDexNum = SpeciesToNationalPokedexNum(species);
+
+    switch(sentToPc)
+    {
+    case MON_GIVEN_TO_PARTY:
+    case MON_GIVEN_TO_PC:
+        GetSetPokedexFlag(nationalDexNum, FLAG_SET_SEEN);
+        GetSetPokedexFlag(nationalDexNum, FLAG_SET_CAUGHT);
+        if (IsMonCSRShiny(mon))
+        {
+            GetSetPokedexFlag(nationalDexNum, FLAG_SET_SHINY_FOUND);
+        }
+        break;
+    }
+
+    Free(mon);
+    return sentToPc;
+}
+
 u8 ScriptGiveEgg(u16 species)
 {
     struct Pokemon *mon = AllocZeroed(sizeof(struct Pokemon));
