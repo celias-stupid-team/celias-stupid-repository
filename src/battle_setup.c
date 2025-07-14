e538c55a83a8cd7d429696b3f86ab2599769e69a
CeliaDawn <mynameisaverb1@gmail.com>
2025-07-14 12:00:06 -0400

    Shiny mime more ifxed

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -238,6 +238,9 @@ static bool8 CheckSilphScopeInPokemonTower(u16 mapGroup, u16 mapNum)
 
 void StartWildBattle(void)
 {
+    if(gSaveBlock1Ptr->location.mapGroup == MAP_GROUP(MAP_ROUTE2) && gSaveBlock1Ptr->location.mapNum == MAP_NUM(MAP_ROUTE2)) {
+        setvar(VAR_ROUTE2_SHINY_MIME, 1);
+    }
     if (GetSafariZoneFlag())
         DoSafariBattle();
     else if (CheckSilphScopeInPokemonTower(gSaveBlock1Ptr->location.mapGroup, gSaveBlock1Ptr->location.mapNum))
