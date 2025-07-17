35164852b776aaec2a80874aa6a6f672141665bd
CeliaDawn <mynameisaverb1@gmail.com>
2025-07-17 16:19:36 -0400

    Burn edits

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -366,7 +366,7 @@
 #define FLAG_DRILLED_RAIN_HOLE               0x140
 #define FLAG_DRILLED_LION_HOLE               0x141
 #define FLAG_CHASED_VULPIX               0x142
-#define FLAG_0x143               0x143
+#define FLAG_GOT_WAKE_UP_SLAP               0x143
 #define FLAG_0x144               0x144
 #define FLAG_0x145               0x145
 #define FLAG_0x146               0x146
