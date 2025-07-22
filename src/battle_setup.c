bc35f4c172d9db5ca17737deaa054fe3d70c62fe
wiz1989 <80073265+wiz1989@users.noreply.github.com>
2025-07-22 17:52:41 +0200

    fixed intro speeches for artificial double battle

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -1150,7 +1150,17 @@ static const u8 *ReturnEmptyStringIfNull(const u8 *string)
 
 static const u8 *GetIntroSpeechOfApproachingTrainer(void)
 {
+    if (gNoOfApproachingTrainers == 2)
+    {
+        return ReturnEmptyStringIfNull(gApproachingTrainers[gApproachingTrainerId].trainerIntroSpeech);
+    }
     return ReturnEmptyStringIfNull(sTrainerAIntroSpeech);
+}
+
+const u8 *GetTrainerAIntroSpeechFromScriptPointer(const u8 *trainerScript)
+{
+    const u8 *introText = (const u8 *)TrainerBattleLoadArg32(trainerScript + 6);
+    return ReturnEmptyStringIfNull(introText);
 }
 
 const u8 *GetTrainerALoseText(void)
