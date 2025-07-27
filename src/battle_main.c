009687d3505012dd6f7ba000298dafcbd143fc40
CeliaDawn <mynameisaverb1@gmail.com>
2025-07-27 10:04:50 -0400

    Paralysis speed minimized

--- src/battle_main.c
+++ src/battle_main.c
@@ -3447,7 +3447,7 @@ u8 GetWhoStrikesFirst(u8 battler1, u8 battler2, bool8 ignoreChosenMoves)
     if (holdEffect == HOLD_EFFECT_MACHO_BRACE)
         speedBattler1 /= 2;
     if (gBattleMons[battler1].status1 & STATUS1_PARALYSIS)
-        speedBattler1 /= 4;
+        speedBattler1 = 1; // Guaranteed outslow Trick Room
     if (holdEffect == HOLD_EFFECT_QUICK_CLAW && gRandomTurnNumber < (0xFFFF * holdEffectParam) / 100)
         speedBattler1 = UINT_MAX;
 
