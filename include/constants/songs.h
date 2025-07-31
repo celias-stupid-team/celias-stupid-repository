9298bf434c533ef6ad403eab9f624c0571ebaa87
CeliaDawn <mynameisaverb1@gmail.com>
2025-07-30 22:13:01 -0400

    Chapter 8 logo

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -394,12 +394,13 @@
 #define MUS_CSR_SECRET_HOUSE            385
 #define MUS_KOROK_SEED            386
 #define MUS_TRICK_HOUSE            387
+#define MUS_CHAPTER_8            388
 
 
 
 #define MUS_NONE                    0xFFFF
 
 #define START_MUS                   MUS_HEAL
-#define END_MUS                     MUS_TRICK_HOUSE
+#define END_MUS                     MUS_CHAPTER_8
 
 #endif  // GUARD_CONSTANTS_SONGS_H
