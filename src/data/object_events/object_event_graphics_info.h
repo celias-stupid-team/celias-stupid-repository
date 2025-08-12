18467b9e6041182b2a9a686ea0ca05f00477ee1d
CeliaDawn <mynameisaverb1@gmail.com>
2025-08-12 18:00:01 -0400

    Silph co 2F start

--- src/data/object_events/object_event_graphics_info.h
+++ src/data/object_events/object_event_graphics_info.h
@@ -4445,7 +4445,7 @@ const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Riley = {
 };
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_DmcaDad = { 
     .tileTag = 0xFFFF,
-    .paletteTag = OBJ_EVENT_PAL_TAG_NPC_PINK,
+    .paletteTag = OBJ_EVENT_PAL_TAG_NPC_WHITE,
     .reflectionPaletteTag = OBJ_EVENT_PAL_TAG_NONE,
     .size = 512,
     .width = 16,
