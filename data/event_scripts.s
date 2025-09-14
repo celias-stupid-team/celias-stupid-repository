fb40afbd3b5920d06778cad59b5cc30dc1e37c2d
CeliaDawn <mynameisaverb1@gmail.com>
2025-09-14 14:36:09 -0400

    gym hub rooms

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1654,3 +1654,33 @@ Text_TestMsg::
 
 	.include "data/maps/TwoIsland_Drugstore/scripts.inc"
 	.include "data/maps/TwoIsland_Drugstore/text.inc"
+
+	.include "data/maps/ViridianGym_HubRoom/scripts.inc"
+	.include "data/maps/ViridianGym_HubRoom/text.inc"
+
+	.include "data/maps/ViridianGym_HubRoom_A/scripts.inc"
+	.include "data/maps/ViridianGym_HubRoom_A/text.inc"
+
+	.include "data/maps/ViridianGym_HubRoom_6/scripts.inc"
+	.include "data/maps/ViridianGym_HubRoom_6/text.inc"
+
+	.include "data/maps/ViridianGym_HubRoom_5/scripts.inc"
+	.include "data/maps/ViridianGym_HubRoom_5/text.inc"
+
+	.include "data/maps/ViridianGym_HubRoom_8/scripts.inc"
+	.include "data/maps/ViridianGym_HubRoom_8/text.inc"
+
+	.include "data/maps/ViridianGym_HubRoom_4/scripts.inc"
+	.include "data/maps/ViridianGym_HubRoom_4/text.inc"
+
+	.include "data/maps/ViridianGym_HubRoom_3/scripts.inc"
+	.include "data/maps/ViridianGym_HubRoom_3/text.inc"
+
+	.include "data/maps/ViridianGym_HubRoom_7/scripts.inc"
+	.include "data/maps/ViridianGym_HubRoom_7/text.inc"
+
+	.include "data/maps/ViridianGym_HubRoom_2/scripts.inc"
+	.include "data/maps/ViridianGym_HubRoom_2/text.inc"
+
+	.include "data/maps/ViridianGym_HubRoom_9/scripts.inc"
+	.include "data/maps/ViridianGym_HubRoom_9/text.inc"
