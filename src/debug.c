c2e98369f25834963ba852615efbcf60eb06b480
Celia Dawn <53879104+CeliaDawn@users.noreply.github.com>
2025-11-13 23:05:04 -0500

    Cynthia battle

--- src/debug.c
+++ src/debug.c
@@ -3768,7 +3768,8 @@ static void DebugAction_Sound_MUS_SelectId(u8 taskId)
     X(MUS_POKEMON_ISLAND_LAB) \
     X(MUS_BINKS_SAKE) \
     X(MUS_HALF_SIGN) \
-    X(MUS_RAINBOW_CASTLE)
+    X(MUS_RAINBOW_CASTLE) \
+    X(MUS_CYNTHIA_BATTLE)
 
 #define SOUND_LIST_SE            \
     X(SE_USE_ITEM)               \
