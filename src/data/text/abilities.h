f1ddfd7ea45f58fce3c338694f28cb41a765de5f
Celia Dawn <53879104+CeliaDawn@users.noreply.github.com>
2025-11-17 19:48:58 -0500

    cynthis room

--- src/data/text/abilities.h
+++ src/data/text/abilities.h
@@ -178,6 +178,7 @@ const u8 *const gAbilityDescriptionPointers[ABILITIES_COUNT] =
     [ABILITY_NEUTRALIZING_GAS] = COMPOUND_STRING("All Abilities are nullified."),
     [ABILITY_HTML5] = COMPOUND_STRING("Outdated as of 2014."),
     [ABILITY_BLIND_AS_A_BAT] = COMPOUND_STRING("Full immunity to LIGHT."),
+    [ABILITY_FRIED_CHICKEN] = COMPOUND_STRING("Can be dipped in ketchup."),
 };
 
 const u8 gAbilityNames[ABILITIES_COUNT][ABILITY_NAME_LENGTH + 1] =
@@ -274,4 +275,5 @@ const u8 gAbilityNames[ABILITIES_COUNT][ABILITY_NAME_LENGTH + 1] =
     [ABILITY_NEUTRALIZING_GAS] = _("NEUTRAL. GAS"),
     [ABILITY_HTML5] = _("HTML4"),
     [ABILITY_BLIND_AS_A_BAT] = _("BLIND AS BAT"),
+    [ABILITY_FRIED_CHICKEN] = _("GHOLDENBROWN"),
 };
--- src/data/trainer_parties.h
+++ src/data/trainer_parties.h
@@ -5243,37 +5243,9 @@ static const struct TrainerMonItemCustomMoves sParty_EliteFourBruno[] = {
     {
         .iv = 250,
         .lvl = 51,
-        .species = SPECIES_ONIX,
+        .species = SPECIES_SUDOWOODO,
         .heldItem = ITEM_NONE,
-        .moves = {MOVE_EARTHQUAKE, MOVE_ROCK_TOMB, MOVE_IRON_TAIL, MOVE_ROAR},
-    },
-    {
-        .iv = 250,
-        .lvl = 53,
-        .species = SPECIES_HITMONCHAN,
-        .heldItem = ITEM_NONE,
-        .moves = {MOVE_SKY_UPPERCUT, MOVE_MACH_PUNCH, MOVE_ROCK_TOMB, MOVE_COUNTER},
-    },
-    {
-        .iv = 250,
-        .lvl = 53,
-        .species = SPECIES_HITMONLEE,
-        .heldItem = ITEM_NONE,
-        .moves = {MOVE_MEGA_KICK, MOVE_FORESIGHT, MOVE_BRICK_BREAK, MOVE_FACADE},
-    },
-    {
-        .iv = 250,
-        .lvl = 54,
-        .species = SPECIES_ONIX,
-        .heldItem = ITEM_NONE,
-        .moves = {MOVE_DOUBLE_EDGE, MOVE_EARTHQUAKE, MOVE_IRON_TAIL, MOVE_SAND_TOMB},
-    },
-    {
-        .iv = 250,
-        .lvl = 56,
-        .species = SPECIES_MACHAMP,
-        .heldItem = ITEM_SITRUS_BERRY,
-        .moves = {MOVE_CROSS_CHOP, MOVE_BULK_UP, MOVE_SCARY_FACE, MOVE_ROCK_TOMB},
+        .moves = {MOVE_ROCK_THROW, MOVE_NONE, MOVE_NONE, MOVE_NONE},
     },
 };
 
