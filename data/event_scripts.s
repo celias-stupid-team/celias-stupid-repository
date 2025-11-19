98b050e051028d55b5302166f378e08655a891e9
Celia Dawn <53879104+CeliaDawn@users.noreply.github.com>
2025-11-19 05:28:02 -0500

    Map Placeholders Made

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1777,3 +1777,36 @@ Text_TestMsg::
 
 	.include "data/maps/Ocean3/scripts.inc"
 	.include "data/maps/Ocean3/text.inc"
+
+	.include "data/maps/AetherParadise_SkullRoom/scripts.inc"
+	.include "data/maps/AetherParadise_SkullRoom/text.inc"
+
+	.include "data/maps/Wormhole_Entrance/scripts.inc"
+	.include "data/maps/Wormhole_Entrance/text.inc"
+
+	.include "data/maps/Wormhole_OrthwormRoom/scripts.inc"
+	.include "data/maps/Wormhole_OrthwormRoom/text.inc"
+
+	.include "data/maps/Wormhole_Exit/scripts.inc"
+	.include "data/maps/Wormhole_Exit/text.inc"
+
+	.include "data/maps/PokemonIsland_LabBackRoom/scripts.inc"
+	.include "data/maps/PokemonIsland_LabBackRoom/text.inc"
+
+	.include "data/maps/RainbowCloud_Transition/scripts.inc"
+	.include "data/maps/RainbowCloud_Transition/text.inc"
+
+	.include "data/maps/RainbowCloud/scripts.inc"
+	.include "data/maps/RainbowCloud/text.inc"
+
+	.include "data/maps/Route25_BillsSecretGarden/scripts.inc"
+	.include "data/maps/Route25_BillsSecretGarden/text.inc"
+
+	.include "data/maps/VermilionCity_FanClubBasement/scripts.inc"
+	.include "data/maps/VermilionCity_FanClubBasement/text.inc"
+
+	.include "data/maps/ThreeIsland_Outside/scripts.inc"
+	.include "data/maps/ThreeIsland_Outside/text.inc"
+
+	.include "data/maps/ThreeIsland_Core/scripts.inc"
+	.include "data/maps/ThreeIsland_Core/text.inc"
