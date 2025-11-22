e6dd3279a51e413c907b7078bcfb9cf0153025e4
Celia Dawn <53879104+CeliaDawn@users.noreply.github.com>
2025-11-21 22:55:35 -0500

    nat park polish

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -320,7 +320,7 @@ It is reset after every battle.
 #define VAR_FUSHCIA_GYM_SHEDINJA_STATE                 0x40DC
 #define VAR_DIGLETTS_CAVE_DIGLETTS                 0x40DD
 #define VAR_LANCE_LASS_ATTACK                 0x40DE
-#define VAR_0x40DF                 0x40DF
+#define VAR_NATIONAL_PARK_ENTRANCE                 0x40DF
 #define VAR_0x40E0                 0x40E0
 #define VAR_0x40E1                 0x40E1
 #define VAR_0x40E2                 0x40E2
--- src/debug.c
+++ src/debug.c
@@ -3776,7 +3776,8 @@ static void DebugAction_Sound_MUS_SelectId(u8 taskId)
     X(MUS_MUS_RINGSIDE) \
     X(MUS_YES) \
     X(MUS_LEDER_SONG) \
-    X(MUS_PRELUDE_TO_FINALE)
+    X(MUS_PRELUDE_TO_FINALE) \
+    X(MUS_NATIONAL_PARK)
 
 #define SOUND_LIST_SE            \
     X(SE_USE_ITEM)               \
