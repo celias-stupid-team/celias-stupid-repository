545bfc32ba9287a645cf96955d7a1964e529b764
RavePossum <ravepossum@proton.me>
2025-12-05 14:12:25 -0700

    Untangle the brick wall situation

--- src/data/object_events/object_event_graphics_info_pointers.h
+++ src/data/object_events/object_event_graphics_info_pointers.h
@@ -387,7 +387,6 @@ const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_dung_beetle;
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_dungless_beetle;
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_absol_2;
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_petrel_2;
-const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_brick_wall;
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_tirtouga;
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_chinchou;
 const struct ObjectEventGraphicsInfo *const gObjectEventGraphicsInfoPointers[NUM_OBJ_EVENT_GFX] = {
@@ -778,7 +777,6 @@ const struct ObjectEventGraphicsInfo *const gObjectEventGraphicsInfoPointers[NUM
     [OBJ_EVENT_GFX_DUNGLESS_BEETLE] = &gObjectEventGraphicsInfo_dungless_beetle,
     [OBJ_EVENT_GFX_ABSOL_2] = &gObjectEventGraphicsInfo_absol_2,
     [OBJ_EVENT_GFX_PETREL_2] = &gObjectEventGraphicsInfo_petrel_2,
-    [OBJ_EVENT_GFX_BRICK_WALL] = &gObjectEventGraphicsInfo_brick_wall,
     [OBJ_EVENT_GFX_TIRTOUGA] = &gObjectEventGraphicsInfo_tirtouga,
     [OBJ_EVENT_GFX_CHINCHOU] = &gObjectEventGraphicsInfo_chinchou,
 };
