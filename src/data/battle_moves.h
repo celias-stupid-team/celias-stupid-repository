812b346d83fbf7489b3e38ea2e42fd82ddd64721
Celia Dawn <53879104+CeliaDawn@users.noreply.github.com>
2026-01-02 12:42:10 -0500

    crunch fixed

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -3208,7 +3208,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .secondaryEffectChance = 20,
         .target = MOVE_TARGET_SELECTED,
         .priority = 0,
-        .flags = FLAG_MAKES_CONTACT | FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED,
+        .flags = FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED,
     },
 
     [MOVE_MIRROR_COAT] =
