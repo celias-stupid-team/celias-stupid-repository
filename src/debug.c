9704b56d20e1b753ef43d6ed3c87b0596c682082
wiz1989 <80073265+wiz1989@users.noreply.github.com>
2026-01-19 18:30:23 +0100

    added debug script to start final battle

--- src/debug.c
+++ src/debug.c
@@ -392,7 +392,7 @@ static const u8 sDebugText_Util_Script_3[] = _("Toggle Rotom Menu");
 static const u8 sDebugText_Util_Script_4[] = _("Script 4");
 static const u8 sDebugText_Util_Script_5[] = _("Script 5");
 static const u8 sDebugText_Util_Script_6[] = _("Script 6");
-static const u8 sDebugText_Util_Script_7[] = _("start Seel battle");
+static const u8 sDebugText_Util_Script_7[] = _("start final battle");
 static const u8 sDebugText_Util_Script_8[] = _("toggle battle PC switch");
 // Util Menu
 static const u8 sDebugText_Util_FlyToMap[] = _("Fly to map…{CLEAR_TEXT_TO 110}{RIGHT_ARROW}");
