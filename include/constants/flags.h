e45dcd199fd877caa1ee73407ebc2079bb3b9ae2
Celia Dawn <53879104+CeliaDawn@users.noreply.github.com>
2026-01-31 21:59:13 -0500

    kenya mroe

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -485,7 +485,7 @@
 #define FLAG_GOT_SHINY_LATIAS                       0x1B2
 #define FLAG_GOT_SHINY_PSYDUCK                 0x1B3
 #define FLAG_NO_SHINY_LOCK                 0x1B4
-#define FLAG_0x1B5                        0x1B5
+#define FLAG_SHINY_KENYA                        0x1B5
 #define FLAG_0x1B6                   0x1B6
 #define FLAG_0x1B7                  0x1B7
 #define FLAG_0x1B8                  0x1B8
