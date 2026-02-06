0586888d9fedae6155e7bf9c530d8439cba4099e
Celia Dawn <53879104+CeliaDawn@users.noreply.github.com>
2026-02-06 00:39:31 -0500

    bofa

--- src/data/pokemon/level_up_learnsets.h
+++ src/data/pokemon/level_up_learnsets.h
@@ -7268,7 +7268,7 @@ static const struct LevelUpMove sDiancieLevelUpLearnset[] = {
 };
 
 static const struct LevelUpMove sHoopaLevelUpLearnset[] = {
-	LEVEL_UP_MOVE(1, MOVE_BELLY_DRUM),
+	LEVEL_UP_MOVE(1, MOVE_IMPRISON),
     LEVEL_UP_MOVE(1, MOVE_TRUMP_CARD),
 	LEVEL_UP_END
 };
