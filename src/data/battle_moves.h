e33a35b4169b061804c837ca6bcc81e4935d2fa8
Celia Dawn <53879104+CeliaDawn@users.noreply.github.com>
2026-03-03 13:59:15 -0500

    spaito phase 2

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -247,6 +247,20 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .flags = FLAG_MAKES_CONTACT | FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGS_ROCK_AFFECTED,
     },
 
+    [MOVE_SINGLE_WINGBEAT] =
+    {
+        .effect = EFFECT_HIT,
+        .power = 60,
+        .type = TYPE_FLYING,
+        .accuracy = 100,
+        .pp = 35,
+        .secondaryEffectChance = 0,
+        .target = MOVE_TARGET_SELECTED,
+        .priority = 0,
+        .flags = FLAG_MAKES_CONTACT | FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGS_ROCK_AFFECTED,
+    },
+
+
     [MOVE_WHIRLWIND] =
     {
         .effect = EFFECT_ROAR,
