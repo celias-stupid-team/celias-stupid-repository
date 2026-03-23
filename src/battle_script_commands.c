b72574c1937b1b5421c677b81ac5267ab47a60b9
Celia Dawn <53879104+CeliaDawn@users.noreply.github.com>
2026-03-23 14:16:47 -0400

    Remove the extra drives breaking things

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -12684,12 +12684,6 @@ void BS_SetTechnoBlastType(void)
 
     if (itemId == ITEM_BURN_DRIVE)
         moveType = TYPE_FIRE;
-    else if (itemId == ITEM_DOUSE_DRIVE)
-        moveType = TYPE_WATER;
-    else if (itemId == ITEM_SHOCK_DRIVE)
-        moveType = TYPE_ELECTRIC;
-    else if (itemId == ITEM_CHILL_DRIVE)
-        moveType = TYPE_ICE;
 
     gBattleStruct->dynamicMoveType = moveType;
 
