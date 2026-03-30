632614c96570e48ff2eaef8f1f13c364edad5061
Celia Dawn <53879104+CeliaDawn@users.noreply.github.com>
2026-03-29 21:43:08 -0400

    celia secrets part whatever

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -10864,7 +10864,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
     [MOVE_SKULL_ISSUE] =
     {
         .effect = EFFECT_HIT,
-        .power = 40,
+        .power = 70,
         .type = TYPE_GHOST,
         .accuracy = 100,
         .pp = 35,
