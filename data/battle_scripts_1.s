bcec730449b60619b57f87a6f6dbb1f6213908cd
wiz1989 <80073265+wiz1989@users.noreply.github.com>
2026-04-01 18:38:02 +0200

    WoW now burns Fire types

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -2394,7 +2394,6 @@ BattleScript_EffectWillOWisp::
 	ppreduce
 	jumpifstatus2 BS_TARGET, STATUS2_SUBSTITUTE, BattleScript_ButItFailed
 	jumpifstatus BS_TARGET, STATUS1_BURN, BattleScript_AlreadyBurned
-	jumpiftype BS_TARGET, TYPE_FIRE, BattleScript_NotAffected
 	jumpifability BS_TARGET, ABILITY_WATER_VEIL, BattleScript_WaterVeilPrevents
 	jumpifstatus BS_TARGET, STATUS1_ANY, BattleScript_ButItFailed
 	accuracycheck BattleScript_ButItFailed, ACC_CURR_MOVE
