c2451eef707b51c8e41302050878eda6fadb34a8
Celia Dawn <53879104+CeliaDawn@users.noreply.github.com>
2026-04-03 08:50:49 -0400

    what are you sinking

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -10669,7 +10669,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
     {
         .effect = EFFECT_HIT,
         .power = 120,
-        .type = TYPE_ELECTRIC,
+        .type = TYPE_STEEL,
         .accuracy = 100,
         .pp = 35,
         .secondaryEffectChance = 10,
