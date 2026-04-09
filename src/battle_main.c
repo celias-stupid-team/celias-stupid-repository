d5bcb8aa33eaacb512ac3c12cc1e2447244463e3
wiz1989 <80073265+wiz1989@users.noreply.github.com>
2026-04-09 22:05:05 +0200

    no prio moves during WEATHER_TRICK_ROOM

--- src/battle_main.c
+++ src/battle_main.c
@@ -3536,6 +3536,7 @@ u8 GetWhoStrikesFirst(u8 battler1, u8 battler2, bool8 ignoreChosenMoves)
     u8 holdEffect = 0;
     u8 holdEffectParam = 0;
     u16 moveBattler1 = 0, moveBattler2 = 0;
+    s8 effectivePriorityBattler1, effectivePriorityBattler2; // for the Trick Room Speed exception
 
     if (WEATHER_HAS_EFFECT)
     {
@@ -3653,11 +3654,22 @@ u8 GetWhoStrikesFirst(u8 battler1, u8 battler2, bool8 ignoreChosenMoves)
             moveBattler2 = MOVE_NONE;
     }
 
+    // treat prio moves as 0 prio during WEATHER_TRICK_ROOM
+    effectivePriorityBattler1 = gBattleMoves[moveBattler1].priority;
+    effectivePriorityBattler2 = gBattleMoves[moveBattler2].priority;
+    if (GetCurrentWeather() == WEATHER_TRICK_ROOM)
+    {
+        if (effectivePriorityBattler1 > 0)
+            effectivePriorityBattler1 = 0;
+        if (effectivePriorityBattler2 > 0)
+            effectivePriorityBattler2 = 0;
+    }
+
     // both move priorities are different than 0
-    if (gBattleMoves[moveBattler1].priority != 0 || gBattleMoves[moveBattler2].priority != 0)
+    if (effectivePriorityBattler1 != 0 || effectivePriorityBattler2 != 0)
     {
         // both priorities are the same
-        if (gBattleMoves[moveBattler1].priority == gBattleMoves[moveBattler2].priority)
+        if (effectivePriorityBattler1 == effectivePriorityBattler2)
         {
             if (speedBattler1 == speedBattler2 && Random() & 1)
                 strikesFirst = 2; // same speeds, same priorities
@@ -3676,7 +3688,7 @@ u8 GetWhoStrikesFirst(u8 battler1, u8 battler2, bool8 ignoreChosenMoves)
                     strikesFirst = 1; // else battler2 has more speed
             }
         }
-        else if (gBattleMoves[moveBattler1].priority < gBattleMoves[moveBattler2].priority)
+        else if (effectivePriorityBattler1 < effectivePriorityBattler2)
             strikesFirst = 1; // battler2's move has greater priority
         // else battler1's move has greater priority
     }
