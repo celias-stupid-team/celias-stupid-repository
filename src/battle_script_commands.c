b6922c699fdc266f5e4bdcbdd26db049c7e62085
wiz1989 <80073265+wiz1989@users.noreply.github.com>
2026-04-12 12:51:42 +0200

    fixed Shiny Dex Flags for transforms

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -4785,7 +4785,11 @@ static void Cmd_playanimation(void)
         // set Pokédex flags
         HandleSetPokedexFlag(SpeciesToNationalPokedexNum(gBattleMons[gActiveBattler].species), FLAG_SET_SEEN, gBattleMons[gActiveBattler].personality);
         if (GetBattlerSide(gActiveBattler) == B_SIDE_PLAYER)
+        {
             HandleSetPokedexFlag(SpeciesToNationalPokedexNum(gBattleMons[gActiveBattler].species), FLAG_SET_CAUGHT, gBattleMons[gActiveBattler].personality);
+            if (IsMonShiny(mon)) // only if the mon is shiny
+                HandleSetPokedexFlag(SpeciesToNationalPokedexNum(gBattleMons[gActiveBattler].species), FLAG_SET_SHINY_FOUND, gBattleMons[gActiveBattler].personality);
+        }
     }
     else if (gHitMarker & HITMARKER_NO_ANIMATIONS)
     {
