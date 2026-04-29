10ed66bbe4136fdf39336ae3be4f25ce8efed18c
KaizenStarlight <starlight6357.business@gmail.com>
2026-04-29 06:24:16 -0600

    Updated Dev battle to just 2 pokemon

--- src/data/trainer_parties.h
+++ src/data/trainer_parties.h
@@ -10604,12 +10604,6 @@ static const struct TrainerMonItemCustomMoves sParty_Starlight[] = {
     {
         .iv = 0,
         .lvl = 50,
-        .species = SPECIES_KRABBY_POKERAP,
-        .moves = {MOVE_CRAB_THROW, MOVE_CAMBRIAN_EXPLOSION, MOVE_CRAB_GRIP, MOVE_HAZE}
-    },
-    {
-        .iv = 0,
-        .lvl = 50,
         .heldItem = ITEM_NONE,
         .species = SPECIES_SWAMPERT,
         .moves = {MOVE_SOFT_LOCK, MOVE_MOONLIGHT_YEAR, MOVE_LANDS_MATH, MOVE_EARTH_POWER}
@@ -10620,14 +10614,7 @@ static const struct TrainerMonItemCustomMoves sParty_Starlight[] = {
         .heldItem = ITEM_NONE,
         .species = SPECIES_GARDEVOIR,
         .moves = {MOVE_FAIRY_FANG, MOVE_SECRET_SWORD, MOVE_DOUBLE_JUMP, MOVE_BOTTOM_SURGERY}
-    }
-    //{
-    //    .iv = 0,
-    //    .lvl = 50,
-    //    .heldItem = ITEM_NONE,
-    //    .species = SPECIES_CHARIZARD,
-    //    .moves = {MOVE_SOFT_LOCK, MOVE_MOONLIGHT_YEAR, MOVE_LANDS_MATH, MOVE_EARTH_POWER}
-    //},
+    },
 };
 
 static const struct TrainerMonItemCustomMoves sParty_Plasto[] = {
