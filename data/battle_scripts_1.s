90723fcd8c37e0702e45a13d1f5a7c273ef33af6
wiz1989 <80073265+wiz1989@users.noreply.github.com>
2026-05-17 18:35:29 +0200

    fail safe for ABILITY_COLOR_CHANGE_WIZ

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -3201,6 +3201,7 @@ BattleScript_FaintedMonTryChoose::
 	jumpifbattletype BATTLE_TYPE_BATTLE_TOWER, BattleScript_FaintedMonSendOutNew
 	jumpifbattletype BATTLE_TYPE_DOUBLE, BattleScript_FaintedMonSendOutNew
 	jumpifword CMP_COMMON_BITS, gHitMarker, HITMARKER_PLAYER_FAINTED, BattleScript_FaintedMonSendOutNew
+	jumpifbattlerside BS_FAINTED, B_SIDE_PLAYER, BattleScript_FaintedMonSendOutNew @ skip switch prompt if player's mon fainted without HITMARKER_PLAYER_FAINTED (e.g. Explosion absorbed by ability)
 	jumpifbyte CMP_EQUAL, sBATTLE_STYLE, OPTIONS_BATTLE_STYLE_SET, BattleScript_FaintedMonSendOutNew
 	jumpifopponenttrainerclass TRAINER_CLASS_RAPPER, BattleScript_FaintedMonSendOutNew
 	jumpifcantswitch BS_PLAYER1, BattleScript_FaintedMonSendOutNew
