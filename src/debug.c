eb34c0cbd166f24ffb57becfc2c145f7751c0622
KaizenStarlight <starlight6357.business@gmail.com>
2026-05-25 12:28:35 -0600

    added higher pitch version of rival encounter

--- src/debug.c
+++ src/debug.c
@@ -3872,6 +3872,7 @@ static void DebugAction_Sound_MUS_SelectId(u8 taskId)
     X(MUS_SE_SPACE_JAM) \
     X(MUS_BW_RIVAL) \
     X(MUS_TINY_LASS) \
+    X(MUS_ENCOUNTER_RIVAL_HIGH)      \
 
 #define SOUND_LIST_SE            \
     X(SE_USE_ITEM)               \
